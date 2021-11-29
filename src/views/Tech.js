0b07f09f7ec22598232fa3b0f2e2e25ee26a5bf7
Joe Gibbs <joseph.gibbs1188@gmail.com>
2021-11-29 17:51:15 -0600

    hard coded tech stack

--- src/views/Tech.js
+++ src/views/Tech.js
@@ -1,43 +1,114 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { getTech } from '../helpers/techData';
-// import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 export default function Tech() {
-  const [tech, setTech] = useState([]);
+  const TechCardTitle = styled.h5`
+    text-align: center;
+  `;
 
-  useEffect(() => {
-    let isMounted = true;
-    getTech().then((techArray) => {
-      if (isMounted) {
-        setTech(techArray);
-      }
-    });
-    return () => {
-      isMounted = false;
-    };
-  }, []);
+  const TechCardPageTitle = styled.h1`
+    text-align: center;
+  `;
 
-  console.warn(tech);
+  const TechCardContainer = styled.div`
+    text-align: center;
+    margin: auto;
+  `;
+
+  const TechCards = styled.div`
+    margin: auto;
+    display: flex;
+    flex-wrap: wrap;
+  `;
+
   return (
     <>
-      <h1>Tech Stack</h1>
       <div>
-        {/* {user ? (
-          <Link className="linkStyling" to="/create">
-            Create a new Tech
-          </Link>
-        ) : (
-          <div />
-        )} */}
+        <div className="tech-div .mx-auto">
+          <TechCardPageTitle>Tech Stack</TechCardPageTitle>
+          <TechCardContainer>
+            <TechCards>
+              <div className="image-div">
+                <TechCardTitle>Postman</TechCardTitle>
+                <img
+                  src="https://cdn.worldvectorlogo.com/logos/postman.svg"
+                  alt="Postman"
+                  style={{
+                    width: '150px',
+                    height: '150px',
+                    border: '1px solid darkblue',
+                  }}
+                />
+              </div>
+              <div className="image-div">
+                <TechCardTitle>Visual Studio Code</TechCardTitle>
+                <img
+                  src="https://cdn.worldvectorlogo.com/logos/visual-studio-code-1.svg"
+                  alt="Visual Studio Code"
+                  style={{ width: '150px', height: '150px' }}
+                />
+              </div>
+              <div className="image-div">
+                <TechCardTitle>Github</TechCardTitle>
+                <img
+                  src="https://cdn.worldvectorlogo.com/logos/github-icon-1.svg"
+                  alt="Github"
+                  style={{ width: '150px', height: '150px' }}
+                />
+              </div>
+              <div className="image-div">
+                <TechCardTitle>Firebase</TechCardTitle>
+                <img
+                  src="https://cdn.worldvectorlogo.com/logos/firebase-1.svg"
+                  alt="Firebase"
+                  style={{ width: '150px', height: '150px' }}
+                />
+              </div>
+              <div className="image-div">
+                <TechCardTitle>CSS</TechCardTitle>
+                <img
+                  src="https://cdn.worldvectorlogo.com/logos/css-3.svg"
+                  alt="CSS"
+                  style={{ width: '150px', height: '150px' }}
+                />
+              </div>
+              <div className="image-div">
+                <TechCardTitle>Git Bash</TechCardTitle>
+                <img
+                  src="https://cdn.worldvectorlogo.com/logos/git-bash.svg"
+                  alt="Git Bash"
+                  style={{ width: '150px', height: '150px' }}
+                />
+              </div>
+              <div className="image-div">
+                <TechCardTitle>HTML 5</TechCardTitle>
+                <img
+                  src="https://cdn.worldvectorlogo.com/logos/html5-2.svg"
+                  alt="HTML 5"
+                  style={{ width: '150px', height: '150px' }}
+                />
+              </div>
+              <div className="image-div">
+                <TechCardTitle>Microsoft Teams</TechCardTitle>
+                <img
+                  src="https://cdn.worldvectorlogo.com/logos/microsoft-teams-1.svg"
+                  alt="Microsoft Teams"
+                  style={{ width: '150px', height: '150px' }}
+                />
+              </div>
+              <div className="image-div">
+                <TechCardTitle>Salesforce</TechCardTitle>
+                <img
+                  src="https://cdn.worldvectorlogo.com/logos/salesforce-2.svg"
+                  alt="Salesforce"
+                  style={{ width: '150px', height: '150px' }}
+                />
+              </div>
+            </TechCards>
+          </TechCardContainer>
+        </div>
       </div>
-      {/* {tech.map((projects) => (
-        // <ProjectCard need to build a tech card or page
-          key={projects.firebaseKey}
-          projects={projects}
-          user={user}
-        />
-      ))} */}
     </>
   );
 }
